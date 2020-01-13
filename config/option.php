e9606f86ef310e2a888029ab9b9927c81dc16b22
nitaa03 <nitarosita0399@gmail.com>
2020-01-13 16:01:58 +0700

    first commit

---
+++ config/option.php
@@ -0,0 +1,3 @@
+<?
+require_once 'koneksi.php';$nama_toko='Toko Istana HP';$meta_deskripsi='Toko Istana HP adalah toko handphone terlengkap dan termurah dari berbagai merk dan tipe';$meta_keyword='Toko handphone terlengkap';$email_pengelola='istana_hp@gmail.com';
+$nomor_hp='08974882346';$nomor_rekening='6356271(BCA)';
